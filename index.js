09fbeadcd29feda0d5b010ea7b55ffd94a3cc970
Michael Albertz <michael@prio.one>
2017-11-19 23:46:43 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+module.exports = {
+  extends: 'airbnb',
+  parser: 'babel-eslint',
+  rules: {
+    // only .jsx files may have JSX
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-filename-extension.md
+    'react/jsx-filename-extension': ['error', { extensions: ['.js'] }],
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'never',
+        objects: 'never',
+        imports: 'never',
+        exports: 'never',
+        functions: 'never'
+      }
+    ],
+    'space-before-function-paren': ['error', 'always'],
+    semi: ['error', 'never'],
+    'max-len': [
+      'error',
+      80,
+      2,
+      {
+        ignoreUrls: true,
+        ignoreComments: false,
+        ignoreRegExpLiterals: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true
+      }
+    ],
+    // enforces new line after each method call in the chain to make it
+    // more readable and easy to maintain
+    // https://eslint.org/docs/rules/newline-per-chained-call
+    'newline-per-chained-call': ['error', { ignoreChainWithDepth: 2 }],
+  }
+}
