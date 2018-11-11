683d7d666adc6925b42fd8b53cd86dd811a5338d
Michael Albertz <michael@prio.one>
2018-11-11 09:39:22 +0100

    split into base and react

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 module.exports = {
   extends: [
-    'eslint-config-airbnb-base',
-    'eslint-config-airbnb-base/rules/strict',
+    './base',
     'eslint-config-airbnb/rules/react',
     'eslint-config-airbnb/rules/react-a11y'
   ].map(require.resolve),
-  parser: 'babel-eslint',
   rules: {
     'jsx-a11y/anchor-is-valid': [
       'error',
@@ -15,7 +13,6 @@ module.exports = {
         aspects: ['noHref', 'invalidHref', 'preferButton']
       }
     ],
-    'import/prefer-default-export': 'off',
     'react/forbid-prop-types': [
       'error',
       {
@@ -25,65 +22,6 @@ module.exports = {
     'react/jsx-one-expression-per-line': false,
     // only .jsx files may have JSX
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-filename-extension.md
-    'react/jsx-filename-extension': ['error', { extensions: ['.js'] }],
-    'comma-dangle': [
-      'error',
-      {
-        arrays: 'never',
-        objects: 'never',
-        imports: 'never',
-        exports: 'never',
-        functions: 'never'
-      }
-    ],
-    'function-paren-newline': 'off',
-    'space-before-function-paren': ['error', 'always'],
-    semi: ['error', 'never'],
-    'max-len': [
-      'error',
-      80,
-      2,
-      {
-        ignoreUrls: true,
-        ignoreComments: true,
-        ignoreRegExpLiterals: true,
-        ignoreStrings: true,
-        ignoreTemplateLiterals: true
-      }
-    ],
-    // enforces new line after each method call in the chain to make it
-    // more readable and easy to maintain
-    // https://eslint.org/docs/rules/newline-per-chained-call
-    'newline-per-chained-call': ['error', { ignoreChainWithDepth: 2 }],
-    // Forbid the use of extraneous packages
-    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-extraneous-dependencies.md
-    // paths are treated both as absolute paths, and relative to process.cwd()
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: [
-          'test/**', // tape, common npm pattern
-          'tests/**', // also common npm pattern
-          'spec/**', // mocha, rspec-like pattern
-          '**/stories/**', // also common npm pattern
-          '**/__tests__/**', // jest pattern
-          'test.{js,jsx}', // repos with a single test file
-          'test-*.{js,jsx}', // repos with multiple top-level test files
-          '**/*.{test,spec}.{js,jsx}', // tests where the extension denotes that it is a test
-          '**/*.{stories,story}.{js,jsx}', // incomponent storybook story
-          '**/jest.config.js', // jest config
-          '**/webpack.config.js', // webpack config
-          '**/webpack.config.*.js', // webpack config
-          '**/rollup.config.js', // rollup config
-          '**/rollup.config.*.js', // rollup config
-          '**/gulpfile.js', // gulp config
-          '**/gulpfile.*.js', // gulp config
-          '**/Gruntfile{,.js}', // grunt config
-          '**/protractor.conf.js', // protractor config
-          '**/protractor.conf.*.js' // protractor config
-        ],
-        optionalDependencies: false
-      }
-    ]
+    'react/jsx-filename-extension': ['error', { extensions: ['.js'] }]
   }
 }
