cd96461a19dec59a83529a7fa0c28e4ca92a53a8
Michael Albertz <michael@prio.one>
2019-09-10 17:04:58 +0200

    tweak no-restricted-syntax rule

--- rules.js
+++ rules.js
@@ -66,6 +66,26 @@ module.exports = {
     ],
     'operator-linebreak': ['error', 'before', { overrides: { '=': 'ignore' } }],
     'no-underscore-dangle': 'off',
+    // add this to remove forof from the list, we use regeneratorRuntime everywhere anyways ...
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ForInStatement',
+        message:
+          'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.'
+      },
+      {
+        selector: 'LabeledStatement',
+        message:
+          'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.'
+      },
+      {
+        selector: 'WithStatement',
+        message:
+          '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.'
+      }
+    ],
+
     'jsx-a11y/anchor-is-valid': [
       'error',
       {
